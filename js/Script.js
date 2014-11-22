a146925e66218d5d832b72a09264d5e4e0266c78
Andres Ramirez <andres_fr.o@hotmail.com>
2014-11-22 23:35:13 +0100

    cambios de admin

--- js/Script.js
+++ js/Script.js
@@ -5,3 +5,50 @@ $('.share').click(function(){
 $('body').keypress(function(){
 	$('.box-light').fadeOut(600);
 });
+
+/*boton admin banner*/
+$('.control-banner').click(function(){
+	$('.banner-box').fadeIn(600);
+});
+
+$('body').keypress(function(){
+	$('.banner-box').fadeOut(600);
+});
+
+/*boton admin producto*/
+$('.control-product').click(function(){
+	$('.product-box').fadeIn(600);
+});
+
+$('body').keypress(function(){
+	$('.product-box').fadeOut(600);
+});
+
+/*boton admin oferta*/
+$('.control-oferta').click(function(){
+	$('.oferta-box').fadeIn(600);
+});
+
+$('body').keypress(function(){
+	$('.oferta-box').fadeOut(600);
+});
+
+/*boton admin noticias*/
+$('.control-news').click(function(){
+	$('.news-box').fadeIn(600);
+});
+
+$('body').keypress(function(){
+	$('.news-box').fadeOut(600);
+});
+
+/*boton ventas*/
+$('.control-ventas').click(function(){
+	$('.ventas-box').fadeIn(600);
+});
+
+$('body').keypress(function(){
+	$('.ventas-box').fadeOut(600);
+});
+
+
